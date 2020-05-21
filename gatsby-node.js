0a7ba52a81b443b80a808a6dfa16386bac6f2c33
vladimirmak02 <vovamak107@gmail.com>
2020-05-21 16:27:45 +0200

    testing 2

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,15 @@
 //     })
 //   })
 // }
+
+const webpack = require(`webpack`)
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    plugins: [
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^netlify-identity-widget$/,
+      }),
+    ],
+  })
+}
